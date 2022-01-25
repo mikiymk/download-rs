b17deea258661a65ca129f2cd6094494423efaf7
miki <mikiymk001@gmail.com>
2022-01-25 20:04:45 +0900

    download and parse html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = get_url();
+    let resp = reqwest::get(&url).await?;
+    let text = resp.text().await?;
+    let document = scraper::Html::parse_document(&text);
+    println!("{:#?}", document);
+    Ok(())
+}
+
+fn get_url() -> String {
+    let mut args = std::env::args();
+    args.next();
+    args.next()
+        .get_or_insert("https://google.com".to_string())
+        .to_owned()
 }
